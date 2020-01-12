663020c9eb534f94e543fa459cadac431a7757c5
Morgan <lockhart.mb@gmail.com>
2020-01-12 16:06:10 -0500

    Fixed warnings

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { FaInstagram } from "react-icons/fa";
 import styles from "./Footer.module.scss";
-import instagram from "../../img/social/instagram.svg";
 
 const Footer = () => {
   return (
@@ -10,7 +9,11 @@ const Footer = () => {
         <h4>Call me today!</h4>
         <p className={styles.phoneNumber}>807-355-2032</p>
         <p>Follow me:</p>
-        <a href="https://www.instagram.com/goodytwospoons/" target="_blank">
+        <a
+          href="https://www.instagram.com/goodytwospoons/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FaInstagram />
         </a>
         <p>© Goody Two Spoons. All rights reserved.</p>
